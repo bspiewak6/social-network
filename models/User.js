606607ad8dc896c2ce0c3791c854d4b7a34063f6
Spiewak <bspiewak6@gmail.com>
2020-12-10 09:42:03 -0500

    fixed routes bug, successful testing of all User CRUD routes in Insomnia

--- models/User.js
+++ models/User.js
@@ -13,24 +13,28 @@ const UserSchema = new Schema({
       required: 'email is required',
       match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
     },
-    thoughts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-        }
-    ],
-    friends: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    ]
+    // thoughts: [
+    //     {
+    //         type: Schema.Types.ObjectId,
+    //         ref: 'Thought'
+    //     }
+    // ],
+    // friends: [
+    //     {
+    //         type: Schema.Types.ObjectId,
+    //         ref: 'User'
+    //     }
+    // ],
+    //     toJSON: {
+    //         virtuals: true
+    //     },
+    //     id: false
 });
 
-// get friend count 
-UserSchema.virtual('friendCount').get(function() {
-    return this.friends.reduce((total, comment) => total + comment.friends.length + 1, 0);
-});
+// virtual to get friend count
+// UserSchema.virtual('friendCount').get(function() {
+//     return this.friends.reduce((total, comment) => total + comment.friends.length + 1, 0);
+// });
   
 // create the User model using the UserSchema
 const User = model('User', UserSchema);
