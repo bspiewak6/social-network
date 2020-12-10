f0339ac2f8ab54272cf05b75d25a8531b6a8d82a
Spiewak <bspiewak6@gmail.com>
2020-12-09 21:00:00 -0500

    updated user-routes and imported, bug on POST route

--- models/User.js
+++ models/User.js
@@ -4,24 +4,21 @@ const UserSchema = new Schema({
     username: {
       type: String,
       unique: true,
-      required: true,
+      required: 'username is required',
       trim: true
     },
     email: {
       type: String,
       unique: true,
-      required: true,
-      validate: {
-        validator: () => Promise.resolve(false),
-        message: 'Email validation failed'
+      required: 'email is required',
+      match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
     },
     thoughts: [
         {
             type: Schema.Types.ObjectId,
             ref: 'Thought'
         }
-    ]
-    },
+    ],
     friends: [
         {
             type: Schema.Types.ObjectId,
